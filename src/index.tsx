4a27d6406809e4b76902b54e1b92f998d5d9bce5
Alexander Vowinkel <https://github.com/kaktus42>
2024-03-07 16:29:16 +0100

    switch to PWA
    PWA = Progressive Web App

--- src/index.tsx
+++ src/index.tsx
@@ -1,48 +1,34 @@
-import { Provider } from 'react-redux';
-import { configureStore } from "@reduxjs/toolkit";
-// import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom/client";
 
-import App from './App';
-import reducer from "./reducers";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-import './index.css';
-import 'onsenui/css/onsenui.css';
-import 'onsenui/css/onsen-css-components.css';
+import App from "./App";
+import { store } from "./reducers";
 
-declare let window: any;
+import "./index.css";
+import "onsenui/css/onsenui.css";
+import "onsenui/css/onsen-css-components.css";
 
-const store = configureStore({
-  reducer,
-  devTools: true
-})
-
-const renderReactDom = () => {
-  const container = document.getElementById('root');
-  const mainJsx = (
-    // <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-    // </React.StrictMode>
-  );
-
-  ReactDOM.render(mainJsx, container);
-
-  // const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-  // root.render(mainJsx);
-};
-
-if (window.cordova) {
-  document.addEventListener('deviceready', () => {
-    renderReactDom();
-  }, false);
-} else {
-  renderReactDom();
-}
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  // <React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // </React.StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,8 +1,16 @@
-import { combineReducers } from '@reduxjs/toolkit'
-import gameSlice from './game'
-import uiSlice from './ui'
+import { configureStore } from "@reduxjs/toolkit";
+import gameSlice from "./game";
+import uiSlice from "./ui";
 
-export default combineReducers<RootState>({
+export const store = configureStore({
+  reducer: {
     ui: uiSlice,
     game: gameSlice,
-})
+  },
+  devTools: true
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
